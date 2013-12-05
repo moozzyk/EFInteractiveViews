62fa891f3342ba1aa5c4c6e04e5a965e7022276c
Pawel Kadluczka <moozzyk@gmail.com>
2013-12-04 23:23:04 -0800

    Updating assembly info

--- InteractivePreGeneratedViews/Properties/AssemblyInfo.cs
+++ InteractivePreGeneratedViews/Properties/AssemblyInfo.cs
@@ -6,18 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("InteractivePreGeneratedViews")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("Interactive Pre-Generated Views for Entity Framework 6")]
+[assembly: AssemblyCompany("Pawel Kadluczka")]
 [assembly: AssemblyProduct("InteractivePreGeneratedViews")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: InternalsVisibleTo("InteractivePregeneratedViewsTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-// TODO:
-//[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
--- InteractivePregeneratedViewsTests/Properties/AssemblyInfo.cs
+++ InteractivePregeneratedViewsTests/Properties/AssemblyInfo.cs
@@ -5,14 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UnitTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyTitle("InteractivePregeneratedViewsTests")]
+[assembly: AssemblyDescription("Tests for Interactive Pre-Generated Views for Entity Framework 6")]
+[assembly: AssemblyCompany("Pawel Kadluczka")]
+[assembly: AssemblyProduct("InteractivePregeneratedViewsTests")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
